f4fc9c31fc83fc3f9ea3e8ec9cdbcce2743e815d
sharann3604 <sharan.n3604@gmail.com>
2026-03-10 13:29:46 +0530

    All files added

---
+++ SNPSU_B1_ECE_EEE/src/com/advanced/dsa/algos/sorting/SelectionSort.java
@@ -0,0 +1,29 @@
+package com.advanced.dsa.algos.sorting;
+
+import java.util.Arrays;
+
+public class SelectionSort {
+    public static void selectionSort(int[] arr) {
+        for (int i=0;i<arr.length-1;i++){
+            int minIndex = i;
+            for (int j=i+1;j<arr.length;j++){
+                if (arr[j]<arr[minIndex]){
+                    minIndex=j;
+                }
+            }
+            if (minIndex!=i){
+                int temp = arr[i];
+                arr[i]= arr[minIndex];
+                arr[minIndex]=temp;
+            }
+        }
+
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {100,3,200,4,-2,5,8,10,1};
+        System.out.println(Arrays.toString(arr));
+        selectionSort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+}
