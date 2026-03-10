9d9d0dfd8db944b5defadd3b58e1da6f5a208fce
sharann3604 <sharan.n3604@gmail.com>
2026-03-10 12:00:41 +0530

    All files added

---
+++ SNPSU_B1_ECE_EEE/src/com/advanced/dsa/algos/searching/MaxMinIntegers.java
@@ -0,0 +1,10 @@
+package com.advanced.dsa.algos.searching;
+
+public class MaxMinIntegers {
+    public static void main(String[] args) {
+        System.out.println("Max Integer Value:"+ Integer.MAX_VALUE);
+        System.out.println("Min Integer Value:"+ Integer.MIN_VALUE);
+        System.out.println("MainInteger +1:" + (Integer.MAX_VALUE+1));
+        System.out.println("MinInteger - 1:"+ (Integer.MIN_VALUE-1));
+    }
+}
